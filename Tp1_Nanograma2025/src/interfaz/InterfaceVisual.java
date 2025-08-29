6990b8d6a16ac179c515baf0229a80876854f7d4
leansan93 <sanchezleandro988@gmail.com>
2025-08-29 17:28:09 -0300

    comentarios de lo que hace los eventos

--- Tp1_Nanograma2025/src/interfaz/InterfaceVisual.java
+++ Tp1_Nanograma2025/src/interfaz/InterfaceVisual.java
@@ -65,25 +65,42 @@ public class InterfaceVisual {
 		frame.getContentPane().add(panelMatriz);
 		
 		JButton btnVerificar = new JButton("verificar");
+		btnVerificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/*  con los botones matriz de la matriz que se pinto del usuario  se usara en el oto evento de "verificar" pero eso sera mas adelante
+				 *  y ahi se comparara la  matriz del usuario con el creado y dira si gano o no */
+				
+			}
+		});
 		btnVerificar.setBounds(10, 227, 89, 23);
 		frame.getContentPane().add(btnVerificar);
 		
 		JButton btnTabla5x5 = new JButton("Tabla 5x5");
 		btnTabla5x5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				/* 
+				 * lo que hay que hacer dentro del evento de crear la tabla con el boton 5x5 
+					es hacer un metodo a parte como clkearTabla()  donde se pueda recibir los clicks y guardarlos  
+					tambien otro a parte que muestre las pistas directamente que esta ya en la parte logica 
+					en los labels
+					
+					
+				*/ 
 				
-				TablaJuego tabla = new TablaJuego (5);
-				int fila = tabla.getTamano();
-				int columna = tabla.getTamano();
-				botones = new JToggleButton [fila][columna];
+				
+				TablaJuego tabla = new TablaJuego (5); // crea desde la logica la tabla con un unico parametro
+				int fila = tabla.getTamano(); // tamno de fila del tablero
+				int columna = tabla.getTamano(); // tamano del tablero 
+				botones = new JToggleButton [fila][columna]; // y creo el boton con esos tamanos 
 				panelMatriz.removeAll(); // limpia antes por si había otra tabla
-			    panelMatriz.setLayout(new GridLayout(fila, columna, 2, 2));
+			    panelMatriz.setLayout(new GridLayout(fila, columna, 2, 2)); 
 
+			    // los recorre y pinta  
 			        for(int fil = 0; fil < fila; fil++) {
 			            for(int col = 0; col < columna; col++) {
 			                botones[fil][col] = new JToggleButton();
 			                botones[fil][col].setOpaque(true);
-			                 
+			         
 			                botones[fil][col].setBackground(Color.WHITE);
 			                panelMatriz.add(botones[fil][col]);
 			                
@@ -94,7 +111,7 @@ public class InterfaceVisual {
 			        panelMatriz.revalidate();
 			        panelMatriz.repaint();
 			    
-			}
+			} 
 		});
 		btnTabla5x5.setBounds(10, 69, 89, 23);
 		frame.getContentPane().add(btnTabla5x5);
